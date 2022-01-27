fcf299cdd600da47d81ee8c0ae5fa408c111fdc8
Daniel G <danielifguerrero@gmail.com>
2022-01-27 16:08:35 -0500

    regular assembly function

--- contracts/GasOptimizer.sol
+++ contracts/GasOptimizer.sol
@@ -7,5 +7,17 @@ contract ContractSize{
                  size := extcodesize(_externalAddress)
         }
     return size;
+    
+
+assembly {
+    
+    function my_assembly_function(param1, param2) -> my_result {
+        
+        // param2 - (4 * param1)
+        my_result := sub(param2, mul(4, param1))
+    
     }
+    let some_value = my_assembly_function(4, 9)  // 4 - (9 * 4) = 32
 }
+}}
+
